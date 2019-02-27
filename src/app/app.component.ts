0cd5a95e7979bb65342eef31a1c2fdeb4a880a15
Rubal Walia <walia.rubal@gmail.com>
2019-02-27 12:52:57 +0530

    Staging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  ngOnInit(): void {
+    AOS.init();
+  }
 }
