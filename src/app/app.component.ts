560fdc8cf2653ced9a178d4b22ac8420dca92867
Rubal Walia <walia.rubal@gmail.com>
2019-02-27 15:40:44 +0530

    Revert "Removed old angular site."
    This reverts commit e5d208c3a9a5b67273d113e97a4989676418e4e2.

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    AOS.init();
+  }
+}
