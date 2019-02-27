e5d208c3a9a5b67273d113e97a4989676418e4e2
Rubal Walia <walia.rubal@gmail.com>
2019-02-27 14:49:40 +0530

    Removed old angular site.

--- src/app/experience/experience.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-experience',
-    templateUrl: './experience.component.html'
-})
-export class ExperienceComponent {
-
-}
--- src/app/portfolio/portfolio.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-portfolio',
-    templateUrl: './portfolio.component.html'
-})
-export class PortfolioComponent {
-
-}
