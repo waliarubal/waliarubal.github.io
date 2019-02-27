f29276edbf002596c8c2176a1548f6e6baa4a561
Rubal Walia <walia.rubal@gmail.com>
2019-02-27 18:50:14 +0530

    Removed useless scripts and styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,34 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     AOS.init();
+
+    // Smooth scroll for links with hashes
+    $('a.smooth-scroll').click(function (event) {
+      // On-page links
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function () {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
+      }
+    });
   }
 }
