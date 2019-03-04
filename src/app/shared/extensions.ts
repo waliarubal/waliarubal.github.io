ce2b41ebe8f3d876bcffb3fcf3a390b862b32bdc
Rubal Walia <walia.rubal@gmail.com>
2019-03-04 11:41:55 +0530

    Staging

--- src/app/shared/extensions.ts
+++ src/app/shared/extensions.ts
@@ -1,8 +1,8 @@
 export class Extensions {
 
-    static Join(joiningChar: string = ',', ...parts: any[]): string {
-        let joinedString = '';
-        parts.forEach(part => joinedString += `${part}${joiningChar} `);
-        return joinedString.length > 0 ? joinedString.substr(0, joinedString.length - (joiningChar.length + 1)) : '';
+    static Join(separator: string, ...parts: any[]): string {
+        let joinedString: string = '';
+        parts.forEach(part => joinedString += `${part}${separator} `);
+        return joinedString.length > 0 ? `${joinedString.substr(0, joinedString.length - separator.length)}` : '';
     }
 }
