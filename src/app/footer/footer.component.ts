1eeb715be62956ca98bd5c1ba9d55572c3f3c301
Rubal Walia <walia.rubal@gmail.com>
2019-03-05 18:21:49 +0530

    Added footer bindings.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '@services/data.service';
+import { BasicInformationModel } from '@shared/models/basic.model';
 
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+    private _basicInfo: BasicInformationModel;
+
+    constructor(private _dataService: DataService) {
+
+    }
+
+    ngOnInit(): void {
+        this._basicInfo = this._dataService.GetBasicInformation();
+    }
+
+    get BasicInformation(): BasicInformationModel {
+        return this._basicInfo;
+    }
 
 }
