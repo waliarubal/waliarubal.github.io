326f3efe52db6728113d6a7d20ee3f3623d21bc3
Rubal Walia <walia.rubal@gmail.com>
2019-03-06 16:46:32 +0530

    Updated project structure for better arrangement of objects.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,35 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from '@app/app.component';
+import { ContentModule } from './content/content.module';
 import { HeaderComponent } from '@app/header/header.component';
 import { FooterComponent } from '@app/footer/footer.component';
-import { ProfileComponent } from '@app/profile/profile.component';
-import { AboutComponent } from '@app/about/about.component';
-import { SkillsComponent } from '@app/skills/skills.component';
-import { PortfolioComponent } from '@app/portfolio/portfolio.component';
-import { ExperienceComponent } from '@app/experience/experience.component';
-import { EducationComponent } from '@app/education/education.component';
-import { ReferencesComponent } from '@app/references/references.component';
-import { ContactComponent } from '@app/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
     HeaderComponent,
     FooterComponent,
-    ProfileComponent,
-    AboutComponent,
-    SkillsComponent,
-    PortfolioComponent,
-    ExperienceComponent,
-    EducationComponent,
-    ReferencesComponent,
-    ContactComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ContentModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/content/content.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { ProfileComponent } from '@app/content/profile/profile.component';
+import { AboutComponent } from '@app/content/about/about.component';
+import { SkillsComponent } from '@app/content/skills/skills.component';
+import { PortfolioComponent } from '@app/content/portfolio/portfolio.component';
+import { ExperienceComponent } from '@app/content/experience/experience.component';
+import { EducationComponent } from '@app/content/education/education.component';
+import { ReferencesComponent } from '@app/content/references/references.component';
+import { ContactComponent } from '@app/content/contact/contact.component';
+import { ContentComponent } from '@app/content/content.component';
+
+@NgModule({
+    declarations: [
+        ProfileComponent,
+        AboutComponent,
+        SkillsComponent,
+        PortfolioComponent,
+        ExperienceComponent,
+        EducationComponent,
+        ReferencesComponent,
+        ContactComponent,
+        ContentComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    exports: [
+        ContentComponent
+    ],
+    providers: []
+})
+export class ContentModule { }
