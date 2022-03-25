1d70d5b87e46723b84972ff98172b849fae17134
Rubal Walia <walia.rubal@gmail.com>
2022-03-25 05:47:11 +0530

    Initial react app code commit.

--- src/app/shared/extensions.ts
+++
@@ -1,9 +0,0 @@
-export class Extensions {
-
-    private static readonly BING_MAP_API_KEY: string = "AvTFCCjxuskfSOPXwR1nbvErOs7It3nBDr-vigU6Bwj1PGL9KlkQHF_5QcMwj3Uv";
-
-    static GetMapImageUrl(latitude: number, longitude: number): string {
-        let url = `https://dev.virtualearth.net/REST/v1/Imagery/Map/Road/${latitude},${longitude}/15?mapSize=1280,680&pushpin=${latitude},${longitude}&format=png&mapMetadata=0&key=${this.BING_MAP_API_KEY}`;
-        return url;
-    }
-}
