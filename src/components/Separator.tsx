6cc51aacc294a5391962e8c596d4ba57a94c9a73
Rubal Walia <walia.rubal@gmail.com>
2022-03-27 05:25:29 +0530

    Static code commit.

---
+++ src/components/Separator.tsx
@@ -0,0 +1,8 @@
+import { Component } from "react"
+
+export class Separator extends Component {
+
+    render() {
+        return <hr className="d-print-none" />;
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { About } from './components/About';
+import { Contact } from './components/Contact';
+import { Cover } from './components/Cover';
+import { Education } from './components/Education';
+import { Experience } from './components/Experience';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { Separator } from './components/Separator';
+import { Skills } from './components/Skills';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <noscript>You need to enable JavaScript to run this website.</noscript>
+    <Header />
+    <div className="page-content">
+      <div className="container">
+        <div className="cover shadow-lg bg-white">
+          <Cover />
+          <About />
+          <Separator />
+          <Skills />
+          <Separator />
+          <Experience />
+          <Separator />
+          <div className="page-break"></div>
+          <Education />
+          <Separator />
+          <Contact />
+        </div>
+      </div>
+    </div>
+    <Footer />
+    <script src="%PUBLIC_URL%/scripts/bootstrap.bundle.min.js?ver=1.2.0"></script>
+    <script src="%PUBLIC_URL%/scripts/aos.js?ver=1.2.0"></script>
+    <script src="%PUBLIC_URL%/scripts/main.js?ver=1.2.0"></script>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('top')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
