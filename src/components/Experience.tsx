2adb3ed7ef901220fac4564cbeaa799310a0e87a
Rubal Walia <walia.rubal@gmail.com>
2022-03-28 01:33:06 +0530

    Added experience card.

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,6 +1,20 @@
 import { Component } from "react";
-import { IWorkExperiences } from "../util/constants";
+import { IWorkExperience, IWorkExperiences } from "../util/constants";
 import { getMonthName } from "../util/helpers";
+
+class ExperienceCard extends Component<IWorkExperience> {
+    render() {
+        return <>
+            <div className="timeline-card timeline-card-primary card shadow-sm" key={this.props.Company}>
+                <div className="card-body">
+                    <div className="h5 mb-1">{this.props.Role} <span className="text-muted h6">at {this.props.Company}</span></div>
+                    <div className="text-muted text-small mb-2">{getMonthName(this.props.From)}, {this.props.From.getFullYear()} - {getMonthName(this.props.To)}, {this.props.To.getFullYear()}</div>
+                    <div>{this.props.Responsibilities}</div>
+                </div>
+            </div>
+        </>;
+    }
+}
 
 export class Experience extends Component<IWorkExperiences> {
 
@@ -9,15 +23,13 @@ export class Experience extends Component<IWorkExperiences> {
             <div className="work-experience-section px-3 px-lg-4">
                 <h2 className="h3 mb-4">Work Experience</h2>
                 <div className="timeline">
-                    {this.props.Experiences.map(experience => <>
-                        <div className="timeline-card timeline-card-primary card shadow-sm" key={experience.Company}>
-                            <div className="card-body">
-                                <div className="h5 mb-1">{experience.Role} <span className="text-muted h6">at {experience.Company}</span></div>
-                                <div className="text-muted text-small mb-2">{getMonthName(experience.From)}, {experience.From.getFullYear()} - {getMonthName(experience.To)}, {experience.To.getFullYear()}</div>
-                                <div>{experience.Responsibilities}</div>
-                            </div>
-                        </div>
-                    </>)}
+                    {this.props.Experiences.map(experience =>
+                        <ExperienceCard
+                            Company={experience.Company}
+                            Role={experience.Role}
+                            From={experience.From}
+                            To={experience.To}
+                            Responsibilities={experience.Responsibilities} />)}
                 </div>
             </div>
         </>;
